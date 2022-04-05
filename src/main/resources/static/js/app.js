64fabac9cf77a58eaf38edfec9ccae475ed69620
Kuba <czubakjakub94@gmail.com>
2022-04-05 19:55:06 +0200

    work on material section

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,11 @@ let sidebarBtn = document.querySelector('#btn');
 let sidebar = document.querySelector(".sidebar");
 let userInfo = document.getElementById("userInfo");
 let materialAddCategoryCancelBtn = document.getElementById("material-add-category-cancel-btn");
+let materialAddItemCancelBtn = document.getElementById("material-add-item-cancel-btn");
 let addMaterialCategorySection = document.getElementById("addMaterialCategory");
+let addMaterialItem = document.getElementById("addMaterialItem");
 let addMaterialCategoryBtn = document.getElementById("add-material-category");
+let createNewMaterial = document.getElementById("createNewMaterial");
 
 
 sidebarBtn.addEventListener("click", function () {
@@ -22,4 +25,12 @@ addMaterialCategorySection.classList.remove("active");
 
 addMaterialCategoryBtn.addEventListener("click", function () {
     addMaterialCategorySection.classList.add("active");
-})
+});
+
+materialAddItemCancelBtn.addEventListener("click", function () {
+addMaterialItem.classList.toggle("active");
+});
+
+createNewMaterial.addEventListener("click", function () {
+    addMaterialItem.classList.toggle("active");
+});
