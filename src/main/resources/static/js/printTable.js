64f9f19d12dbfbfad9ee8ccd74da00ebc17a64eb
Kuba <czubakjakub94@gmail.com>
2022-04-28 17:57:07 +0200

    add printTable feature

--- src/main/resources/static/js/printTable.js
+++ src/main/resources/static/js/printTable.js
@@ -3,13 +3,28 @@ const printBtn = document.getElementById("material_pdf");
 
 
 $(printBtn).click(function () {
-    let materialsTable = document.getElementById("materials-table");
-    let wme = window.open("","","width=900,height=700");
-
-    wme.document.write(materialsTable.outerHTML);
-    wme.document.close();
-    wme.focus();
-    wme.print();
-    wme.close();
+    let table = document.getElementById("materials-table");
+    $(table).printThis({
+        debug: false,               // show the iframe for debugging
+        importCSS: true,            // import parent page css
+        importStyle: false,         // import style tags
+        printContainer: true,       // print outer container/$.selector
+        loadCSS: "",                // path to additional css file - use an array [] for multiple
+        pageTitle: "",              // add title to print page
+        removeInline: false,        // remove inline styles from print elements
+        removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
+        printDelay: 333,            // variable print delay
+        header: null,               // prefix to html
+        footer: null,               // postfix to html
+        base: false,                // preserve the BASE tag or accept a string for the URL
+        formValues: true,           // preserve input/form values
+        canvas: false,              // copy canvas content
+        doctypeString: 'MATERIAL LIST:',       // enter a different doctype for older markup
+        removeScripts: false,       // remove script tags from print content
+        copyTagClasses: false,      // copy classes from the html & body tag
+        beforePrintEvent: null,     // function for printEvent in iframe
+        beforePrint: null,          // function called before iframe is filled
+        afterPrint: null            // function called before iframe is removed
+    });
 });
 
