668f0fc700b64b9cbe2c62dbd36b3e49d1386d42
Kuba <czubakjakub94@gmail.com>
2022-05-05 14:23:57 +0200

    add dropdown menu

--- src/main/resources/static/js/notificationDropdown.js
+++ src/main/resources/static/js/notificationDropdown.js
@@ -1,5 +1,6 @@
 const notificationDropdown = document.getElementById("notificationDropdown");
 const notificationButton = document.getElementById('notificationButton');
+const notificationCounter = document.getElementById('icon-button__badge');
 
 notificationButton.addEventListener('click', function () {
     notificationDropdown.classList.toggle('active');
