f7b251a8aa9b0cd00a39bd6ac8706038b6792be3
Kuba <czubakjakub94@gmail.com>
2022-05-06 10:29:57 +0200

    hide notification when clicked outside container

--- src/main/resources/static/js/notificationDropdown.js
+++ src/main/resources/static/js/notificationDropdown.js
@@ -5,3 +5,10 @@ const notificationCounter = document.getElementById('icon-button__badge');
 notificationButton.addEventListener('click', function () {
     notificationDropdown.classList.toggle('active');
 });
+
+document.addEventListener('mouseup', function(e) {
+
+    if (!notificationDropdown.contains(e.target)) {
+            notificationDropdown.classList.remove('active');
+    }
+});
