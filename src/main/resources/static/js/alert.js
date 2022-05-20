5dc17ce4bcc107313205d21034fe1821f0466aaa
Kuba <czubakjakub94@gmail.com>
2022-05-20 19:12:51 +0200

    change layout

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -12,20 +12,18 @@ const alert_info = document.getElementById('alert_info');
 const alert_success = document.getElementById('alert_success');
 
 
-
-
-
-
 function showErrorAlert(text) {
     alert_error_msg.innerText = text;
     alert_error.classList.remove('hide');
     alert_error.classList.add('show');
 }
+
 function showInfoAlert(text) {
     alert_info_msg.innerText = text;
     alert_info.classList.remove('hide');
     alert_info.classList.add('show');
 }
+
 function showSuccessAlert(text) {
     alert_success_msg.innerText = text;
     alert_success.classList.remove('hide');
@@ -36,19 +34,16 @@ function hideErrorAlert() {
     alert_error.classList.remove('show');
     alert_error.classList.add('hide');
 }
+
 function hideInfoAlert() {
     alert_info.classList.remove('show');
     alert_info.classList.add('hide');
 }
+
 function hideSuccessAlert() {
     alert_success.classList.remove('show');
     alert_success.classList.add('hide');
 }
-
-
-
-
-
 
 alert_error_close_btn.addEventListener('click', function () {
     hideErrorAlert();
@@ -61,22 +56,26 @@ alert_success_close_btn.addEventListener('click', function () {
 });
 
 
-//Check if param "error, logout" exist
+function checkIfErrorOrLogoutParamExist(){
 
-let url_string = window.location.href;
-let url = new URL(url_string);
-let error = url.searchParams.get('error');
-let logout = url.searchParams.get('logout');
+    let url_string = window.location.href;
+    let url = new URL(url_string);
+    let error = url.searchParams.get('error');
+    let logout = url.searchParams.get('logout');
 
-if (error != null) {
-    showErrorAlert('Incorrect email or password!');
-    setTimeout(function () {
-        hideErrorAlert();
-    }, 5000); //hide alert automatically after 5sec
+    if (error != null) {
+        showErrorAlert('Incorrect email or password!');
+        setTimeout(function () {
+            hideErrorAlert();
+        }, 5000); //hide alert automatically after 5sec
+    }
+    if (logout != null) {
+        showInfoAlert('Logout successful');
+        setTimeout(function () {
+            hideInfoAlert();
+        }, 5000); //hide alert automatically after 5sec
+    }
 }
-if (logout != null) {
-    showInfoAlert('Logout successful');
-    setTimeout(function () {
-        hideInfoAlert();
-    }, 5000); //hide alert automatically after 5sec
-}
+
+checkIfErrorOrLogoutParamExist();
+
