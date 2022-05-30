fd19c47b028e6ecaf2a172164ca7ff804471904b
Kuba <czubakjakub94@gmail.com>
2022-05-30 21:10:04 +0200

    fix printTable feature

--- src/main/resources/static/js/printTable.js
+++ src/main/resources/static/js/printTable.js
@@ -4,26 +4,7 @@ const printBtn = document.getElementById("material_pdf");
 $(printBtn).click(function () {
     let table = document.getElementById("materials-table");
     $(table).printThis({
-        debug: true,               // show the iframe for debugging
-        importCSS: true,            // import parent page css
-        importStyle: true,         // import style tags
-        printContainer: true,       // print outer container/$.selector
-        loadCSS: "/css/app.css",                // path to additional css file - use an array [] for multiple
-        pageTitle: "",              // add title to print page
-        removeInline: false,        // remove inline styles from print elements
-        removeInlineSelector: "*",  // custom selectors to filter inline styles. removeInline must be true
-        printDelay: 0,            // variable print delay
-        header: '<h3>MATERIAL LIST:</h3>',               // prefix to html
-        footer: null,               // postfix to html
-        base: false,                // preserve the BASE tag or accept a string for the URL
-        formValues: true,           // preserve input/form values
-        canvas: false,              // copy canvas content
-        doctypeString: '',       // enter a different doctype for older markup
-        removeScripts: false,       // remove script tags from print content
-        copyTagClasses: false,      // copy classes from the html & body tag
-        beforePrintEvent: null,     // function for printEvent in iframe
-        beforePrint: null,          // function called before iframe is filled
-        afterPrint: null            // function called before iframe is removed
+    header: '<h3>MATERIAL LIST:</h3>'
     });
 });
 
